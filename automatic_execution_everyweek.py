527c75c4a1f2d1f7438d177164193fc9c2cade3f
Peter Song <dapsjj@qq.com>
2018-03-27 11:08:23 +0800

    Update automatic_execution_everyweek.py

--- automatic_execution_everyweek.py
+++ automatic_execution_everyweek.py
@@ -44,7 +44,7 @@ def cmp_noun_list(data):
     cmp_nouns = mecab.parse(data)
     cmp_nouns = cmp_nouns.split(" ")
     for every_keyword in cmp_nouns:
-        if (len(every_keyword.strip())==1 and (every_keyword in MULTIBYTE_MARK or every_keyword in JP_HIRA or every_keyword in JP_KATA)):
+        if (every_keyword is not None and len(every_keyword.strip())==1 and (every_keyword in MULTIBYTE_MARK or every_keyword in JP_HIRA or every_keyword in JP_KATA)):
             deltxt_list.append(every_keyword)
         else:
             savetxt_list.append(every_keyword)
@@ -83,29 +83,6 @@ def list2dict(list_data):
         else:
             dict_data[data] = 1
     return dict_data
-
-
-def score_lr(frequency, ignore_words=None, average_rate=1, lr_mode=1, dbm=None):
-    """
-    専門用語とそれを構成する単名詞の情報から重要度を計算する
-        cmp_noun
-            複合語（単名詞の空白区切り）をキーに出現回数を値に
-            したディクショナリ
-        ignore_word
-            重要度計算の例外とする語のリスト
-        average_rate
-            重要度計算においてLRとFrequencyの比重を調整する
-            数値が小さいほうがLRの比重が大きい
-        lr_mode
-            1のときはLRの計算において「延べ数」をとる
-            2のときはLRの計算において「異なり数」をとる
-    """
-    # 対応する関数を呼び出し
-    if dbm is None:
-        noun_importance = _score_lr_dict(frequency, ignore_words, average_rate, lr_mode)
-    else:
-        noun_importance = _score_lr_dbm(frequency, ignore_words, average_rate, lr_mode, dbm)
-    return noun_importance
 
 
 def score_lr(frequency, ignore_words=None, average_rate=1, lr_mode=1, dbm=None):
@@ -220,29 +197,6 @@ def _score_lr_dict(frequency, ignore_words, average_rate=1, lr_mode=1):
         importance = importance ** (1 / (2 * average_rate * count))
         noun_importance[cmp_noun] = importance
         count = 0
-    return noun_importance
-
-
-def score_lr(frequency, ignore_words=None, average_rate=1, lr_mode=1, dbm=None):
-    """
-    専門用語とそれを構成する単名詞の情報から重要度を計算する
-        cmp_noun
-            複合語（単名詞の空白区切り）をキーに出現回数を値に
-            したディクショナリ
-        ignore_word
-            重要度計算の例外とする語のリスト
-        average_rate
-            重要度計算においてLRとFrequencyの比重を調整する
-            数値が小さいほうがLRの比重が大きい
-        lr_mode
-            1のときはLRの計算において「延べ数」をとる
-            2のときはLRの計算において「異なり数」をとる
-    """
-    # 対応する関数を呼び出し
-    if dbm is None:
-        noun_importance = _score_lr_dict(frequency, ignore_words, average_rate, lr_mode)
-    else:
-        noun_importance = _score_lr_dbm(frequency, ignore_words, average_rate, lr_mode, dbm)
     return noun_importance
 
 
@@ -442,7 +396,8 @@ def generate_allweeks_employeelist_data(employee_teacher_list, report_week_list,
     :param report_week_list:top报告周列表
     :return:要插入到report_est_automatic表的list
     '''
-    if len(employee_teacher_list)>0 and len(report_week_list)>0 and (report_year is not None) :
+    # if len(employee_teacher_list)>0 and len(report_week_list)>0 and (report_year is not None) :
+    if employee_teacher_list and report_week_list and report_year:
         report_year = report_year#年
         report_week_list = report_week_list#周列表
         employee_teacher_list = employee_teacher_list#社员号+教师号列表
@@ -460,12 +415,12 @@ def generate_allweeks_employeelist_data(employee_teacher_list, report_week_list,
                         add_list = [str(report_year),str(week),str(employee_teacher[0]),employee_teacher[1],importance_degree,matching_degree,average_score]
                         report_est_automatic_list.append(add_list)
         return report_est_automatic_list
-    else:
-        logger.error("Call method generate_allweeks_employeelist_data() error!"
-                 +" Length of employee_teacher_list:"+str(len(employee_teacher_list))
-                 +" Length of report_week_list:"+str(len(report_week_list))
-                 +" Value of report_year:"+str(report_year))
-        raise
+    # else:
+    #     logger.error("Call method generate_allweeks_employeelist_data() error!"
+    #              +" Length of employee_teacher_list:"+str(len(employee_teacher_list))
+    #              +" Length of report_week_list:"+str(len(report_week_list))
+    #              +" Value of report_year:"+str(report_year))
+        # raise
 
 
 def get_employee_list(server, user, password, database, report_year = datetime.datetime.now().year):
@@ -582,7 +537,7 @@ def calculate_intercept_coef(server, user, password, database, coef_week_list, c
     :param coef_employee_list:生成切片数据(切片(误差)、重要度、マッチ度)所使用的社员号列表
     :return:切片(误差)、重要度、マッチ度
     '''
-    if (report_year is not None) and len(coef_week_list)>0 and len(coef_employee_list)>0:
+    if report_year and coef_week_list and coef_employee_list:
         try:
             conn = pymssql.connect(server, user, password, database)
             cur = conn.cursor()
@@ -601,7 +556,7 @@ def calculate_intercept_coef(server, user, password, database, coef_week_list, c
                     coef_list.append(list(row))
             else:
                 logger.error("coef_list is null. Can not query any row form table report_est_automatic.")
-                raise
+                # raise
         except pymssql.Error as ex:
             logger.error("dbException:" + str(ex))
             raise ex
@@ -611,7 +566,8 @@ def calculate_intercept_coef(server, user, password, database, coef_week_list, c
             raise ex
         finally:
             conn.close()
-        if len(coef_list)>0:
+        # if len(coef_list)>0:
+        if coef_list:
             df = pd.DataFrame(coef_list)
             X = df.iloc[:, 0:2]
             y = df.iloc[:, [-1]]
@@ -620,9 +576,9 @@ def calculate_intercept_coef(server, user, password, database, coef_week_list, c
             return clf.intercept_[0], clf.coef_[0][0], clf.coef_[0][1]#返回:切片(误差)、重要度、マッチ度
         else:
             return None,None,None#如果coef_list为空，返回3个None
-    else:
-        logger.error("Call method calculate_intercept_coef() error!"+"Length of coef_week_list:"+str(len(coef_week_list))+" Length of coef_employee_list:"+str(len(coef_employee_list)))
-        raise
+    # else:
+    #     logger.error("Call method calculate_intercept_coef() error!"+"Length of coef_week_list:"+str(len(coef_week_list))+" Length of coef_employee_list:"+str(len(coef_employee_list)))
+        # raise
 
 
 def insert_report_est_automatic(server, user, password, database, datalist):
@@ -634,7 +590,8 @@ def insert_report_est_automatic(server, user, password, database, datalist):
     :param datalist:要插入数据的列表
     :return:无
     '''
-    if len(datalist)>0:
+    # if len(datalist)>0:
+    if datalist:
         try:
             conn = pymssql.connect(server, user, password, database)
             cur = conn.cursor()
@@ -661,9 +618,9 @@ def insert_report_est_automatic(server, user, password, database, datalist):
             raise ex
         finally:
             conn.close()
-    else:
-        logger.error("Call method insert_report_est_automatic() error!Can not insert into table report_est_automatic! Length of datalist:"+str(len(datalist)))
-        raise
+    # else:
+    #     logger.error("Call method insert_report_est_automatic() error!Can not insert into table report_est_automatic! Length of datalist:"+str(len(datalist)))
+        # raise
 
 
 def insert_report_coefficient(server, user, password, database,teacher_list,intercept,importance_degree,matching_degree,report_year = datetime.datetime.now().year,report_week = [datetime.datetime.now().isocalendar()[1]]):
@@ -699,9 +656,9 @@ def insert_report_coefficient(server, user, password, database,teacher_list,inte
             raise ex
         finally:
             conn.close()
-    else:
-        logger.error("Call method insert_report_coefficient() error!Can not insert into table report_coefficient! deviation: "+str(intercept)+" importance_degree:"+str(importance_degree)+" matching_degree:"+str(matching_degree))
-        raise
+    # else:
+    #     logger.error("Call method insert_report_coefficient() error!Can not insert into table report_coefficient! deviation: "+str(intercept)+" importance_degree:"+str(importance_degree)+" matching_degree:"+str(matching_degree))
+        # raise
 
 
 def calculate_automatic_marking(server, user, password, database, intercept, importance_degree, matching_degree,report_year = datetime.datetime.now().year,report_week = [datetime.datetime.now().isocalendar()[1]]):#计算"自动采点"
@@ -748,9 +705,9 @@ def calculate_automatic_marking(server, user, password, database, intercept, imp
             raise ex
         finally:
             conn.close()
-    else:
-        logger.error("Call method calculate_automatic_marking() error!Can not query from report_est_automatic! deviation: " + str(intercept) + " importance_degree:" + str(importance_degree) + " matching_degree:" + str(matching_degree))
-        raise
+    # else:
+    #     logger.error("Call method calculate_automatic_marking() error!Can not query from report_est_automatic! deviation: " + str(intercept) + " importance_degree:" + str(importance_degree) + " matching_degree:" + str(matching_degree))
+        # raise
 
 
 def update_col_automatic_marking(server, user, password, database,update_employee_list):#更新report_est_automatic表的automatic_marking
@@ -763,7 +720,8 @@ def update_col_automatic_marking(server, user, password, database,update_employe
     :return:无
     '''
     need_to_update_list = update_employee_list
-    if len(need_to_update_list)>0:
+    # if len(need_to_update_list)>0:
+    if need_to_update_list:
         try:
             conn = pymssql.connect(server, user, password, database)
             cur = conn.cursor()
@@ -789,9 +747,9 @@ def update_col_automatic_marking(server, user, password, database,update_employe
             raise ex
         finally:
             conn.close()
-    else:
-        logger.error("Call method update_col_automatic_marking() error!Can not update table report_est_automatic! Length of update_employee_list: " + str(len(update_employee_list)))
-        raise
+    # else:
+    #     logger.error("Call method update_col_automatic_marking() error!Can not update table report_est_automatic! Length of update_employee_list: " + str(len(update_employee_list)))
+        # raise
 
 
 def delete_current_data_from_report_est_automatic(server, user, password, database,report_year = datetime.datetime.now().year,report_week = [datetime.datetime.now().isocalendar()[1]]):
@@ -894,7 +852,8 @@ def insert_auto_pick_point_to_report_est(server, user, password, database,insert
     :return:无
     '''
     need_to_insert_list = insert_auto_pick_point_list
-    if len(need_to_insert_list)>0:
+    # if len(need_to_insert_list)>0:
+    if need_to_insert_list:
         try:
             conn = pymssql.connect(server, user, password, database)
             cur = conn.cursor()
@@ -919,9 +878,9 @@ def insert_auto_pick_point_to_report_est(server, user, password, database,insert
             raise ex
         finally:
             conn.close()
-    else:
-        logger.error("Call method insert_auto_pick_point_to_report_est() error!Can not insert into table report_est! Length of insert_auto_pick_point_list: " + str(len(insert_auto_pick_point_list)))
-        raise
+    # else:
+        # logger.error("Call method insert_auto_pick_point_to_report_est() error!Can not insert into table report_est! Length of insert_auto_pick_point_list: " + str(len(insert_auto_pick_point_list)))
+        # raise
 
 
 def insert_not_exist_person_in_report_target_to_report_est(server, user, password, database,report_year = datetime.datetime.now().year,report_week = [datetime.datetime.now().isocalendar()[1]]):#当周没提交top报告的人需要向report_est表插入一条check_code为90001038,automatic_marking为0的数据
@@ -1014,14 +973,20 @@ if __name__=="__main__":
     read_dateConfig_file_set_year_week()#读配置文件设置report_year和coef_week_list
     # employee_list = get_employee_list(server, user, password, database, report_year)#获取全员的员工列表(返回社员号列表)
     coef_employee_list = get_employee_list_from_table_report_target(server, user, password, database)#从report_target表获取员工列表(返回社员号列表)
+    logger.info("coef_employee_list:"+str(coef_employee_list))
     coef_employee_and_teacher_list = get_employee_and_teacher_list_from_table_report_target(server, user, password, database)#从report_target表获取员工号和教师号列表(返回社员号和教师号列表)
+    logger.info("coef_employee_and_teacher_list:" + str(coef_employee_and_teacher_list))
     delete_current_data_from_report_est_automatic(server, user, password, database, report_year, coef_week_list)#从report_est_automatic表删除当前周数据
+    logger.info("delete_current_data_from_report_est_automatic ok!")
     data_addto_report_est_automatic = generate_allweeks_employeelist_data(coef_employee_and_teacher_list, coef_week_list,report_year)#生成X年、X周、社员号X、教师数据的社员号X、重要度X、匹配度X、平均分X的列表
+    logger.info("data_addto_report_est_automatic:" + str(data_addto_report_est_automatic))
     insert_report_est_automatic(server, user, password, database, data_addto_report_est_automatic)#插入到report_est_automatic
     delete_current_data_from_report_coefficient(server, user, password, database, report_year, coef_week_list)#从report_coefficient表删除当前周数据
     intercept_coef = calculate_intercept_coef(server, user, password, database, coef_week_list, coef_employee_list, report_year)#获取切片、重要度、マッチ度
+    logger.info("intercept_coef:" + str(intercept_coef))
     insert_report_coefficient(server, user, password, database,teacher_list,intercept_coef[0],intercept_coef[1],intercept_coef[2],report_year,coef_week_list)#插入report_coefficient表数据
     automatic_marking_list = calculate_automatic_marking(server, user, password, database,intercept_coef[0],intercept_coef[1],intercept_coef[2],report_year,coef_week_list)#计算自动采点
+    logger.info("automatic_marking_list:" + str(automatic_marking_list))
     # update_col_automatic_marking(server, user, password, database,automatic_marking_list)#更新report_est_automatic表的automatic_marking(自动采点)
     delete_current_data_from_report_est(server, user, password, database, report_year, coef_week_list)  # 从report_est表删除当前周数据
     insert_auto_pick_point_to_report_est(server, user, password, database,automatic_marking_list)#插入自动采点的90001038到report_est表
